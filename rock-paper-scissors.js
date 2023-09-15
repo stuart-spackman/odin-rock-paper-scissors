e097b32b2958c3fb1460fa9ac8851618595ff1ff
Stuart Spackman <stuart.spackman@gmail.com>
2023-09-15 11:49:08 -0700

    it works!

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,49 +1,67 @@
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
+buttons.forEach(button => button.addEventListener('click', playRound));
+buttons.forEach(button => button.addEventListener('click', checkScore));
+
 function getComputerChoice() {
     return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    const editedSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if (editedSelection === 'Rock' && computerSelection === 'Paper') {
-        return `You lose! ${computerSelection} beats ${editedSelection.toLowerCase()}.`;
-    } else if (editedSelection === 'Paper' && computerSelection === 'Scissors') {
-        return `You lose! ${computerSelection} beats ${editedSelection.toLowerCase()}.`;
-    } else if (editedSelection === 'Scissors' && computerSelection === 'Rock') {
-        return `You lose! ${computerSelection} beats ${editedSelection.toLowerCase()}.`;
-    } else if (editedSelection === 'Rock' && computerSelection === 'Scissors') {
-        return `You win! ${editedSelection} beats ${computerSelection.toLowerCase()}.`
-    } else if (editedSelection === 'Scissors' && computerSelection === 'Paper') {
-        return `You win! ${editedSelection} beats ${computerSelection.toLowerCase()}.`
-    } else if (editedSelection === 'Paper' && computerSelection === 'Rock') {
-        return `You win! ${editedSelection} beats ${computerSelection.toLowerCase()}.`
+function playRound() {
+    const playerSelection = this.innerText;
+    const computerSelection = getComputerChoice();
+    const results = document.querySelector('.results');
+    const p = document.createElement('p');
+    p.style.fontWeight = 'bold';
+    p.style.textAlign = 'center';
+
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. ${computerSelection} beats ${playerSelection.toLowerCase()}. You lose! `;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. ${computerSelection} beats ${playerSelection.toLowerCase()}. You lose! `;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. ${computerSelection} beats ${playerSelection.toLowerCase()}. You lose! `;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. ${playerSelection} beats ${computerSelection.toLowerCase()}. You win!`
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. ${playerSelection} beats ${computerSelection.toLowerCase()}. You win! `
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. ${playerSelection} beats ${computerSelection.toLowerCase()}. You win! `
     } else {
-        return `Both parties chose ${editedSelection.toLowerCase()}. This round ends in a draw.`
+        p.innerText = `Computer chose ${computerSelection.toLowerCase()}. This round ends in a draw.`
     }
+    results.appendChild(p);
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 1; i <= 5; i++) {
-        let playerSelection = prompt(`Round ${i}. Make your choice.`);
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result.includes('win')) {
-            playerScore++;
-        } else if (result.includes('lose')) {
-            computerScore++;
-        }
-        console.log(result);
+function checkScore() {
+    const results = document.querySelector('.results');
+    const lastChild = results.lastChild.innerText;
+    if (lastChild.includes('win')) {
+        playerScore++;
+    } else if (lastChild.includes('lose')) {
+        computerScore++;
     }
 
-    if (playerScore > computerScore) {
-        console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}\nYou win!`)
-    } else if (computerScore > playerScore) {
-        console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}\nYou lose!`)
-    } else {
-        console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}\nWe have a draw!`)
+    const update = document.createElement('p');
+    update.style.textAlign = 'center';
+    update.innerText = `Player has ${playerScore} points. Computer has ${computerScore} points.`
+    results.appendChild(update);
+
+    const final = document.createElement('p');
+    final.style.fontSize = '66px';
+    if (playerScore === 3) {
+        final.innerText = "Player wins!";
+        results.appendChild(final);
+        buttons.forEach(button => button.replaceWith(
+            button.cloneNode(true)
+        ));
+    } else if (computerScore === 3) {
+        final.innerText = "Computer wins!";
+        results.appendChild(final);
+        buttons.forEach(button => button.replaceWith(
+            button.cloneNode(true)
+        ));
     }
 }
-
-game();
